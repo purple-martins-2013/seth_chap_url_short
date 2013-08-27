cbe0d9ece7027718bdbf2d05e405fce6a53d06cb
Apprentice <apprentice@devbootcamp.com>
2013-08-26 17:24:47 -0700

    show route

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SethChapUrlShortener::Application.config.secret_token = '02f349a6a7ceab8e5be4d56488c82815486be491b0b97c4a0431ea8456ebee4164c8cf07261a7e60889a1fbb6614ab3372c772fd97f88b8c8fd8632c685fa3dd'
